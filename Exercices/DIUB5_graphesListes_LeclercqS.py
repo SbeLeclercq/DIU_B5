672fc46c49b050d6e70936be989b0c135fb9d7d1
Sébastien Leclercq <leclercq.sebastien@gmail.com>
2020-06-19 21:37:24 +0200

    On avance

--- Exercices/DIUB5_graphesListes_LeclercqS.py
+++ Exercices/DIUB5_graphesListes_LeclercqS.py
@@ -6,7 +6,10 @@ Created on Tue Jun 16 16:13:25 2020
 @author: Sébastien LECLERCQ
 SNT/NSI Lycée Louis Bertrand - Briey
 """
-
+##################################################################
+##Première remarque, j'ai travaillé mercredi ;) 
+##Je ne mets pas les exercices "papier"
+##################################################################
 def ajoutNoeud(graphe,val,etat='I'):
     '''
     Effet : Création d'un noeud et ajout au graphe passé en paramètre
@@ -42,21 +45,21 @@ def ajoutArete(graphe,dep,arr,info=1,bijec=False,etat='I'):
 
 #Création du graphe (une liste vide)
 graphe=[]
+graphe2=[]
+
 #Ajout d'un graphe perso
-ajoutNoeud(graphe,'A')
-ajoutNoeud(graphe,'B')
-ajoutNoeud(graphe,'C')
-ajoutNoeud(graphe,'D')
-ajoutArete(graphe,'A','B',1,True)
-ajoutArete(graphe,'B','C',1,True)
-ajoutArete(graphe,'C','A',1,True)
-ajoutArete(graphe,'A','D',1,True)
-print(graphe)
+ajoutNoeud(graphe2,'A')
+ajoutNoeud(graphe2,'B')
+ajoutNoeud(graphe2,'C')
+ajoutNoeud(graphe2,'D')
+ajoutArete(graphe2,'A','B',1,True)
+ajoutArete(graphe2,'B','C',1,True)
+ajoutArete(graphe2,'C','A',1,True)
+ajoutArete(graphe2,'A','D',1,True)
 
 
 
 
-'''
 #Ajout des noeuds (page 81 graphe de gauche)
 ajoutNoeud(graphe,'A')
 ajoutNoeud(graphe,'B')
@@ -73,11 +76,14 @@ ajoutArete(graphe,'B','C',1,True)
 ajoutArete(graphe,'C','E',7,True)
 ajoutArete(graphe,'C','D',3,True)
 ajoutArete(graphe,'D','E',2,True)
-'''
+
 #Fonctions annexes :
 
 def valNoeud(noeud):
     return(noeud[0])
+
+def areteNoeud(noeud):
+    return(noeud[2])
 
 def valArete(arete):
     return(arete[0])
@@ -97,7 +103,7 @@ def etatVisite(graphe,dep,ar):
     '''
     for noeud in graphe :
         if valNoeud(noeud)==dep or valNoeud(noeud)==ar:
-            for arete in noeud[1]:
+            for arete in areteNoeud(noeud):
                 if valArete(arete)==dep or valArete(arete)==ar:
                     arete[2]='V'
 
@@ -109,9 +115,9 @@ def etatRetour(graphe,dep,ar):
     '''
     for noeud in graphe :
         if valNoeud(noeud)==dep or valNoeud(noeud)==ar:
-            for arete in noeud[1]:
+            for arete in areteNoeud(noeud):
                 if valArete(arete)==dep or valArete(arete)==ar:
-                    arete[2]='R'   
+                    arete[2]='R'
                     
 def etatDecouvert(graphe,val):
     '''
@@ -123,17 +129,59 @@ def etatDecouvert(graphe,val):
         if valNoeud(noeud)==val:
             noeud[1]='D'
 
-etatDecouvert(graphe,'A')
-etatVisite(graphe,'A','B')
-etatDecouvert(graphe,'B')
-etatVisite(graphe,'B','C')
-etatDecouvert(graphe,'C')
-etatRetour(graphe,'A','C')
+
+#Parcours en profondeur
+'''Remarque : Mon id est une lettre donc le choix des chemins est aleatoire
+On peut ajouter des contraintes sur le poids des chemins.
+'''
+
+def parcoursProfondeur(graphe,Id):
+    '''
+    Effet :parcours en profondeur du graphe avec changement d'état.
+    Entrée :
+    Sortie :
+    '''
+    etatDecouvert(graphe,Id)
+    for noeud in graphe :
+        if valNoeud(noeud)==Id :
+            for arete in areteNoeud(noeud) :
+                if etatArete(arete)=='I' :
+                    for arrive in graphe :
+                        if valNoeud(arrive)==valArete(arete) :
+                            if etatNoeud(arrive) == 'I' :
+                                etatVisite(graphe,valNoeud(Id),valNoeud(arrive))
+                                parcoursProfondeur(graphe,valNoeud(arrive))
+                            else :
+                                etatRetour(graphe,valNoeud(Id),valNoeud(arrive))
+    
+def parcoursProfondeur2(graphe,Id,liste=[]):
+    '''
+    Effet : parcours en profondeur du graphe.
+    Entrée :
+    Sortie :La liste dans l'ordre des noeuds visités.
+    '''
+    etatDecouvert(graphe,Id)
+    liste.append(Id)
+    for noeud in graphe :
+        if valNoeud(noeud)==Id :
+            for arete in areteNoeud(noeud) :
+                if etatArete(arete)=='I' :
+                    for arrive in graphe :
+                        if valNoeud(arrive)==valArete(arete) :
+                            if etatNoeud(arrive) == 'I' :
+                                etatVisite(graphe,valNoeud(Id),valNoeud(arrive))
+                                parcoursProfondeur2(graphe,valNoeud(arrive),liste)
+                            else :
+                                etatRetour(graphe,valNoeud(Id),valNoeud(arrive))
+    return(liste)
+
+                                
+                                
+
+
+print(parcoursProfondeur2(graphe,'A'))
 
 print(graphe)
-
-
-        
     
     
 
